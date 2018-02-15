b0037f1a9b417e71bf02793c1a38b9ebdf6f975f
Honchar Denys <crackeraki@gmail.com>
2018-02-15 14:07:56 +0200

    first try of creating simple routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/*
+*	Routing
+*/
+import { RouterModule, Routes} from '@angular/router';
+import { ExploreComponent } from './user/explore/explore.component';
+const app_routing: Routes = [{
+	path: 'explore',
+	component: ExploreComponent
+}];
 
 import { AppComponent } from './app.component';
 
@@ -8,11 +17,10 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  ], imports: [
+    BrowserModule,
+    RouterModule.forRoot(app_routing)
+  ], providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExploreComponent } from './user/explore/explore.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+ ExploreComponent
+  ]
+})
+export class UserModule { }
