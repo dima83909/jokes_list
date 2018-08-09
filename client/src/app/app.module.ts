473bbb0685aa4305059dbff705d2f853cb18605a
Honchar Denys <crackeraki@gmail.com>
2018-08-09 17:16:59 +0300

    fixed issues with user service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchPipe } from './pipes/search.pipe';
-import { ProfileComponent } from './pages/profile/profile.component';
 import { PictureComponent } from './com/modal/picture/picture.component';
 import { ExploreComponent } from './pages/explore/explore.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [{
   path: '',
@@ -15,8 +15,6 @@ const routes: Routes = [{
   path: 'Profile',
   component: ProfileComponent
 }];
-
-
 
 @NgModule({
   declarations: [
