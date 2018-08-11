d1b9f41dbb98414c52f3a6d0c3200a564edbd205
dima83909 <dima83909@gmail.com>
2018-08-11 12:43:29 -0700

    filled page explore with users, added form for user update on profile page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SearchPipe } from './pipes/search.pipe';
 import { PictureComponent } from './com/modal/picture/picture.component';
@@ -27,7 +27,8 @@ const routes: Routes = [{
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import {MongoService} from 'wacom';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
   public users;
+  
+  update(){
+    this.mongo.updateAll('user',{
+      gender: this.gender,
+      name: this.name,
+      birth: this.birth,
+      skills: this.skills,
+      data: this.data
+    });
+  }
 
-  constructor(private mongo: MongoService) {
-    console.log('testing service');
-
+  constructor(private mongo: MongoService, http: HttpClient) {
+    var self = this;
     this.users=mongo.get('user');
-    console.log(this.users);
-    
+    console.log(this.users); 
+    http.get('/api/user/me').subscribe(resp => {
+      for(var key in resp){
+        self[key]=resp[key];
+      }
+    });
+  
   }
 }
