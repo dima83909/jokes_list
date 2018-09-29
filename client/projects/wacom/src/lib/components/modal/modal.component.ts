f84fe7b304b38f567760f9ec8a3d33ab5eef9c1a
Honchar Denys <crackeraki@gmail.com>
2018-09-29 11:25:44 +0300

    added  back on init in wacom modal c

--- client/projects/wacom/src/lib/components/modal/modal.component.ts
+++ client/projects/wacom/src/lib/components/modal/modal.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'modal',
 	templateUrl: './modal.component.html',
 	styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements {
+export class ModalComponent implements OnInit {
 	private id;
 	public full;
 	public cover;
 	public header;
 	public content;
 	constructor(){}
+	ngOnInit(){}
 	private modalClose: Subject<any> = new Subject();
 	close(){
 		this.modalClose.next();
