599e59bd1bc72bf30a4e9fb8418484e96cf167b7
Lytvyniuk <dima83909@gmail.com>
2020-08-31 11:24:06 +0300

    test alert service - need css import

--- client/src/app/pages/guest/login/login.component.ts
+++ client/src/app/pages/guest/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { NgxIzitoastService } from 'ngx-izitoast';
 import { Router } from '@angular/router';
-import { HashService, HttpService } from 'wacom';
+import { HashService, HttpService, AlertService } from 'wacom';
 
 @Component({
 	selector: 'app-login',
@@ -12,38 +11,44 @@ export class LoginComponent {
 	constructor(private router: Router,
 		private hash: HashService,
 		private http: HttpService,
-		private alert: NgxIzitoastService) {
+		private alert: AlertService) {
 		this.user.email = this.hash.get('email')||'ceo@webart.work';
 		this.user.password = this.hash.get('password')||'asdasdasdasd';
 		if(this.hash.get('login')) this.login();
 	}
 	public user:any = {};
 	login() {
+		console.log(this.alert.show);
 		if(!this.user.email) {
-			return this.alert.error({
-				title: 'Enter your email',
+			return this.alert.show({
+				type: "error",
+				text: 'Enter your email',
+				position: "center"
 			});			
 		}
 		this.hash.set('email', this.user.email);
 		if(!this.user.password) {
-			return this.alert.error({
-				title: 'Enter your password',
+			return this.alert.show({
+				type: "error",
+				text: 'Enter your password'
 			});
 		}
 		this.http.post('/api/user/status', this.user, (resp:any) => {
 			if(resp.email && resp.pass) {
 				this.http.post('/api/user/login', this.user, (user:any) => {
 					if(!user){
-						return this.alert.error({
-							title: "Something went wrong",
+						return this.alert.show({
+							type: "error",
+							text: "Something went wrong",
 						});
 					}
 					localStorage.setItem('waw_user', JSON.stringify(user));
 					this.router.navigate(['/profile'])
 				})
 			} else {
-				this.alert.error({
-					title: "Wrong email or password",
+				this.alert.show({
+					type: "error",
+					text: "Wrong email or password"
 				});
 			}
 		})
